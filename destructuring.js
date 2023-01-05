940b4fb04e6cb85c7462b9a77ece286ef30ddde8
Miry Konishi <miry.konishi@gmail.com>
2023-01-05 15:42:20 -0300

    Destructuring assignment completed.

---
+++ destructuring.js
@@ -0,0 +1,97 @@
+// Problema 1:
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar);
+console.log(otherRandomCar);
+// Expected Output:
+  // "Tesla"
+  // "Mercedes"
+// Output: 
+  // "Tesla"
+  // "Mercedes"
+
+
+// Problema 2:
+const employee = {
+  name: 'Elon',
+  age: 47,
+  company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+// Expected output:
+  // Error: variable name doesn't exist (name not definded). Script crashes in this line.
+  // If console.log(otherName) ran correctly output "Elon";
+// Output:
+  // ReferenceError: name is not defined  
+
+
+// Problem 3:
+const person = {
+  name: 'Phil Smith',
+  age: 47,
+  height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+// Expected output:
+  // '12345'
+  // null or undefined
+// Output: 
+  // 12345
+  // undefined
+
+
+// Problem 4:
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+// Notes: 
+// first = 2
+// second = 5
+// third = 2
+// Expected output:
+  // first == second => false
+  // first == third => true
+// Output:
+  // false
+  // true
+
+
+// Problem 5:
+const lastTest = {
+  key: 'value',
+  secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key); 
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+// Expected output:
+  // 'value'
+  // [1,5,1,8,3,3]
+  // 1
+  // 5
+// Output:
+  // value
+  // [ 1, 5, 1, 8, 3, 3 ]
+  // 1
+  // 5
+
+
+
